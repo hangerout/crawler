389bab3db1fa3ce9b63f2f05361f4ca33cfb858b
Hangerout <tenaz3.comp@gmail.com>
2013-08-21 23:37:39 -0300

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Crawler::Application.config.secret_key_base = 'd8bc7401e6d5f4d6a16a82e41e3f36e8f313da512adea77f1a2648056a3d220af0fa8d6f2be0bbe519ad3785ef200b80d78341e725f6b7d7ef67ec0bb1e61d7b'
